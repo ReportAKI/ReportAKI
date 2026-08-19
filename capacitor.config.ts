618c68eedfbabca4d3efe78766ee45652ede8857
eltzanopoulou-code <el.tzanopoulou@gmail.com>
2026-08-19 15:02:14 +0300

    update code

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.reportakiapp',
+  appName: 'reportaki_app',
+  webDir: 'web', // ή ανάλογα που χτίζει το web σου
+};
+export default config;
